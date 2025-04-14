bb662e5a033830c4dee857803f7829adfffd6410
sayedhamed3-ga <sayedhamed2004@gmail.com>
2025-04-14 15:35:14 +0300

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 import './App.css'
 import {Routes ,Route} from 'react-router'
-import LoginForm from './components/LogInForm/LoginForm'
-import SignUpForm from './components/SignUpForm/SignupForm'
-import HomePage from './components/HomePage/HomePage'
-import Navbar from './components/NavBar/NavBar'
+import Login from './pages/Login'
+import Homepage from './pages/Homepage'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 import ValidateIsLoggedIn from './validators/ValidateIsLoggedIn'
 import ValidateIsLoggedOut from './validators/ValidateIsLoggedOut'
-
+import Detailspage from './pages/Detailspage'
+import * as detailServices from './Services/detailservices.js'
+import { useContext, useState, useEffect } from 'react';
+import {authContext} from './context/AuthContext.jsx'
 function App() {
+  const { user } = useContext(authContext);
 
-
+  useEffect(() => {
+    const fetchAllUsers = async () => {
+      const userData = await detailServices.index();
+  
+      // console log to verify
+      console.log('userData:', userData);
+    };
+    if (user) fetchAllUsers();
+  }, [user]);
   return (
     <>
       <Navbar/>
       <Routes>
-        <Route path="/signup" element={<SignUpForm/>}/>
-        <Route path="/login" element={<LoginForm/>}/>
-        <Route path="/home" element={<ValidateIsLoggedIn><HomePage/></ValidateIsLoggedIn>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/home" element={<ValidateIsLoggedIn><Homepage/></ValidateIsLoggedIn>}/>
+        <Route path="/user" element={<ValidateIsLoggedIn><Detailspage/></ValidateIsLoggedIn>}/>
       </Routes>
     </>
   )
